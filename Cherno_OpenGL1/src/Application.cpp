f14a3d23cb144fc5d58030d66164019357e23f95
RedTarantula <joaopedrothmann@hotmail.com>
2019-03-11 16:03:54 -0300

    Basic Shader

--- Cherno_OpenGL1/src/Application.cpp
+++ Cherno_OpenGL1/src/Application.cpp
@@ -1,6 +1,111 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+/*
+CG Class
+Universidade do Rio dos Sinos - Porto Alegre, RS. Brazil
+Professor Rossana Queiroz
+---
+João Pedro Rothmann
+---
+Following TheChernoProject's Youtube series as support
+https://www.youtube.com/watch?v=W3gAzLwfIP0&index=1&list=PLlrATfBNZ98foTJPJ_Ev03o2oq3-GGOS2
+*/
+
+struct ShaderProgram
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+bool fileExists(const std::string& filename)
+{
+	struct stat buf;
+	if (stat(filename.c_str(), &buf) != -1)
+	{
+		return true;
+	}
+	return false;
+}
+
+static ShaderProgram ParseShader(const std::string& filepath)
+{
+	std::ifstream stream(filepath);
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+	int numLines = 0;
+	while (getline(stream,line))
+	{
+		numLines++;
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos)
+			{
+				type = ShaderType::VERTEX;
+			}
+			else if (line.find("fragment") != std::string::npos)
+			{
+				type = ShaderType::FRAGMENT;
+			}
+		}
+		else
+		{
+			ss[(int)type] << line << '\n';
+		}
+	}
+	std::cout << "Lines: " << numLines << std::endl;
+	return{ ss[0].str(),ss[1].str() };
+}
+
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*)alloca(length*sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << message << std::endl;
+		glDeleteShader(id);
+		return 0;
+	}
+	return id;
+}
+
+static int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
+
 
 int main(void)
 {
@@ -25,17 +130,46 @@ int main(void)
 	if (glewInit() != GLEW_OK)
 		std::cout << "Error! GLEW is not okay." << std::endl;
 
+	/* Print the version */
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	unsigned int buffer;
+	float positions[6] = {
+		-.5f,-.5f,
+		.0f, .5f,
+		.5f, -.5f
+	};
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float),positions,GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+	ShaderProgram source = ParseShader("res/shaders/Test.shader");
+	std::cout << "VERTEX" << std::endl;
+	std::cout << source.VertexSource << std::endl;
+	std::cout << "FRAGMENT" << std::endl;
+	std::cout << source.FragmentSource << std::endl;
+
+	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+	glUseProgram(shader);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		
+		/*
 		glBegin(GL_TRIANGLES);
 		glVertex2f(-.5f,-.5f);
 		glVertex2f(.0f, .5f);
 		glVertex2f(.5f, -.5f);
 		glEnd();
+		*/
 
 
 
@@ -45,6 +179,8 @@ int main(void)
 		/* Poll for and process events */
 		glfwPollEvents();
 	}
+
+	glDeleteProgram(shader);
 
 	glfwTerminate();
 	return 0;
